850099cc801700ae41820681931c669013a56b1d
Stefan Allchorne <allchorne39@sky.com>
2024-10-12 18:23:49 +0100

    replace commas

--- site/src/scripts/kebabify.js
+++ site/src/scripts/kebabify.js
@@ -6,7 +6,7 @@ const setupKebabify = (container) => {
     const kebabify = function (string) {
         return String(string)
             .normalize('NFKD').replace(/[\u0300-\u036F]/g, '')
-            .replace(/[?!&’'"]/g, '')
+            .replace(/[?!&’'",]/g, '')
             .replace(/[\s_.:]+/g, '-')
             .toLowerCase();
     };
